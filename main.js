2dd3464a3bd2b5123d95fe05d138aca67982f192
gtitov <gherman.s.titov@gmail.com>
2022-02-27 00:21:14 +0300

    Add points outside of map

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const map = new maplibregl.Map({
 
 map.getCanvas().style.cursor = 'pointer';
 
-layers = ['landUse', 'waterLines', 'waterPolygons', 'contours', 'roadsBlur']
+layers = ['landUse', 'waterLines', 'waterPolygons', 'contours', 'roadsBlur', 'stars-blur1']
 
 map.on('click', (e) => {
   const coordinates = e.lngLat
@@ -32,9 +32,120 @@ map.on('click', (e) => {
     if (feature.properties.RiverName) description += feature.properties.RiverName + "<br>"
     if (feature.properties.Value_) description += "Горизонталь " + feature.properties.Value_ + " м<br>"
     if (feature.sourceLayer == "roads") description += "Дорога<br>"
+    if (feature.source == "stars") description += "Terra Incognita"
   });
   new maplibregl.Popup()
     .setLngLat(coordinates)
     .setHTML(description)
     .addTo(map);
 });
+
+const stars = {
+  'type': 'MultiPoint',
+  'coordinates': [[
+    36.42036437988281,
+    55.20023061331437
+  ], [
+    36.3237190246582,
+    55.19425406139583
+  ],
+  [
+    36.31702423095703,
+    55.19944685432541
+  ],
+  [
+    36.320457458496094,
+    55.21795903813508
+  ],
+  [
+    36.33831024169922,
+    55.18710061539658
+  ],
+  [
+    36.38603210449219,
+    55.185532565186726
+  ],
+  [
+    36.372642517089844,
+    55.18719861648563
+  ],
+  [
+    36.41366958618164,
+    55.211495444210776
+  ],
+  [
+    36.42036437988281,
+    55.20023061331437
+  ],
+  [
+    36.413841247558594,
+    55.22412785315607
+  ],
+  [
+    36.311187744140625,
+    55.18974656018768
+  ],
+  [
+    36.413326263427734,
+    55.23372188680824
+  ],
+  [
+    36.422595977783196,
+    55.18651260379994
+  ],
+  [
+    36.299686431884766,
+    55.211299561341384
+  ],
+  [
+    36.429805755615234,
+    55.210907792710714
+  ],
+  [
+    36.31135940551758,
+    55.22765246093339
+  ]
+  ]
+};
+
+map.on('load', () => {
+  // Add a source and layer displaying a point which will be animated in a circle.
+  map.addSource('stars', {
+    'type': 'geojson',
+    'data': stars
+  });
+
+  map.addLayer({
+    'id': 'stars-blur2',
+    'source': 'stars',
+    'type': 'circle',
+    'paint': {
+      'circle-radius': 10,
+      'circle-opacity': 0.5,
+      'circle-blur': 1,
+      'circle-color': '#FFFF00'
+    }
+  });
+
+  map.addLayer({
+    'id': 'stars-blur1',
+    'source': 'stars',
+    'type': 'circle',
+    'paint': {
+      'circle-radius': 5,
+      'circle-opacity': 0.5,
+      'circle-blur': 1,
+      'circle-color': '#FFFF00'
+    }
+  });
+
+  map.addLayer({
+    'id': 'stars-core',
+    'source': 'stars',
+    'type': 'circle',
+    'paint': {
+      'circle-radius': 2,
+      'circle-color': '#ffffff'
+    }
+  });
+})
